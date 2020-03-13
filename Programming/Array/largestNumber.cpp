d1a60626cc2409d5f71e71e2932780c3577963a5
Vanshika Rastogi <vanshikarastogi7417@gmail.com>
2020-03-13 11:15:20 +0530

    *max_element(arr,arr+n)

--- Programming/Array/largestNumber.cpp
+++ Programming/Array/largestNumber.cpp
@@ -1,18 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// int largeNumber(int arr[],int n)
+// {
+//     int maximum=arr[0];
+//     for(int i=1;i<n;i++)
+//     {
+//         if(arr[i]>maximum){
+//             maximum=arr[i];
+//         }
+//     }
+//     return maximum;
+// }
 int largeNumber(int arr[],int n)
 {
-    int maximum=arr[0];
-    for(int i=1;i<n;i++)
-    {
-        if(arr[i]>maximum){
-            maximum=arr[i];
-        }
-    }
-    return maximum;
+    int maximum=*max_element(arr,arr+n);
 }
-
 int main()
 {
     #ifndef ONLINE_JUDGE 
