0176aa5edaf6c7b94451723c162c29c5d9794b2b
Vanshika Rastogi <vanshikarastogi7417@gmail.com>
2020-03-13 11:09:55 +0530

    largestNumber

---
+++ Programming/Array/largestNumber.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int largeNumber(int arr[],int n)
+{
+    int maximum=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]>maximum){
+            maximum=arr[i];
+        }
+    }
+    return maximum;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE 
+    freopen("input.txt", "r", stdin); 
+    freopen("output.txt", "w", stdout); 
+    #endif 
+
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<largeNumber(arr,n);
+}
