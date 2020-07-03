b7848caa5bbda423295dce1d567a93de337f4ba8
Vanshika Rastogi <40449891+Vanshi8998@users.noreply.github.com>
2020-07-04 00:31:55 +0530

    Update beginner.cpp

--- Programming/beginner.cpp
+++ Programming/beginner.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 
-int sum(int arr[],int n){
+int sum(int a[],int n){
     int sum=0;
     for(int i=0;i<n;i++)
     {
-        sum=sum+arr[i];
+        sum=sum+a[i];
     }
     return sum;
 }
